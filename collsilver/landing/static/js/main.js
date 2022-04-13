a0b491b7451a31f373cacf3fa1cee57ebe510cb5
David Kuenka <cuencaldd2001.29@gmail.com>
2022-04-13 18:46:42 +0300

    Merged with front fixes

--- collsilver/landing/static/js/main.js
+++ collsilver/landing/static/js/main.js
@@ -1,61 +1,30 @@
 $(function(){
-	//Preloader
-	 var $preloader = $('#page-preloader'),
+    //Preloader
+     var $preloader = $('#page-preloader'),
         $spinner   = $preloader.find('.spinner');
     $spinner.fadeOut();
     $preloader.delay(300).fadeOut('slow');
 
-	//Hamburger menu
-	$('.hamburger').click(function(){
-		$('.menu-collapse').toggleClass('d-none').css('order', '1');
-		$('.nav__list').toggleClass('nav__list_opened');
-		$('.hamburger').toggleClass('white');
-	});
-
-    //Modal window SEND_MSG
-	$('#msgbtn').click(function(e){
-		e.preventDefault();
-		$('#sendMsgModal').arcticmodal();
-	});
-	$('#answer').click(function(e){
-		e.preventDefault();
-		$('#sendMsgModal').arcticmodal();
-	});
-
-	//Modal window PHONE
-	$('#tel-btn').click(function(e){
-		e.preventDefault();
-		$('#exampleModal').arcticmodal();
-	});
-	$('.consult__btn').click(function(e){
-		e.preventDefault();
-		$('#exampleModal').arcticmodal();
-	});
-
-    //Modal window AUTHORIZE
-	$('#authorization').click(function(e){
-		e.preventDefault();
-		$('#authorizeModal').arcticmodal();
-	});
-
-    //Modal window REGISTRATION
-	$('#registration').click(function(e){
-		e.preventDefault();
-		$('#registrationModal').arcticmodal();
-	});
-
-    //Slider
-    $('.services-slider').slick({
-        dots: true,
-        prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
-        nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
-        autoplay: true,
-        autoplaySpeed: 2500
+    //Hamburger menu
+    $('.hamburger').click(function(){
+        $('.menu-collapse').toggleClass('d-none').css('order', '1');
+        $('.nav__list').toggleClass('nav__list_opened');
+        $('.hamburger').toggleClass('white');
     });
-});	
+
+    //Modal window PHONE
+    $('#order-btn').click(function(e){
+        e.preventDefault();
+        $('#order-modal').arcticmodal();
+    });
+    $('#desc-order-btn').click(function(e){
+        e.preventDefault();
+        $('#order-modal').arcticmodal();
+    });
+}); 
 
 var linkNav = document.querySelectorAll('[href^="#"]'), //выбираем все ссылки к якорю на странице
-    V = 0.4;  // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
+    V = 0.15;  // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
 for (var i = 0; i < linkNav.length; i++) {
     linkNav[i].addEventListener('click', function(e) { //по клику на ссылку
         e.preventDefault(); //отменяем стандартное поведение
@@ -78,27 +47,57 @@ for (var i = 0; i < linkNav.length; i++) {
     }, false);
 }
 
+
+
 // Border for input
+const MIN_INPUT_COUNTER = 0;
+const MAX_INPUT_COUNTER = 3500;
+const CURRENT_COST = 225;
+
 const inputs = document.querySelectorAll('input[type=number]');
 Array.from(inputs).forEach(input => {
-    const min = 0;
-    const max = 99;
-
     input.addEventListener('input', (e) => {
         const value = +input.value;
-        if (value > max) { input.value = max }
-        else if (value < min) { input.value = min }
+        if (value > MAX_INPUT_COUNTER) { input.value = MAX_INPUT_COUNTER }
+        else if (value < MIN_INPUT_COUNTER) { input.value = MIN_INPUT_COUNTER }
     })
 });
 
 // Order total
-var payment_amount = document.getElementById("payment_amount");
-payment_amount.oninput = function() {
+var payment_amount = document.getElementById("id_order_count");
+payment_amount.onchange = function() {
 
     var amount = parseInt(payment_amount.value);
-    var cost = 225;
+    var cost = CURRENT_COST;
     var total = cost * amount;
-
     var total_output = document.getElementById("payment_total");
     total_output.innerHTML = total;
 }
+
+
+
+// Counter
+var counterDisplayElem = document.querySelector('.counter-display');
+var counterMinusElem = document.querySelector('.counter-minus');
+var counterPlusElem = document.querySelector('.counter-plus');
+var count = MIN_INPUT_COUNTER;
+updateDisplay();
+counterPlusElem.addEventListener("click",()=>{
+    count++;
+    updateDisplay();
+});
+counterMinusElem.addEventListener("click",()=>{
+    count--;
+    updateDisplay();
+});
+function updateDisplay(){
+    if (count > MAX_INPUT_COUNTER) { count = MAX_INPUT_COUNTER }
+    if (count < MIN_INPUT_COUNTER) { count = MIN_INPUT_COUNTER }
+    counterDisplayElem.innerHTML = count;
+    payment_amount.value = count;
+
+    var cost = CURRENT_COST;
+    var total = cost * count;
+    var total_output = document.getElementById("payment_total");
+    total_output.innerHTML = total;
+};
