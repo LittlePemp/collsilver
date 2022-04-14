161eb916f4096309716c8c6cf825f2aebcb295b2
David Kuenka <cuencaldd2001.29@gmail.com>
2022-04-15 02:50:34 +0300

    Debug False

--- collsilver/collsilver/settings.py
+++ collsilver/collsilver/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')78sm)3p6fok(0#bbdt-nki8a6g5qi3@f^8(xack6&f1&9iq2g'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = [os.getenv('HOST'), 'web']
 INSTALLED_APPS = [
     'django.contrib.auth',
