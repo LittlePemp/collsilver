72feb15e151bf6687f2ac90c73093908936c3e29
slonikin211 <slonikin211@gmail.com>
2022-04-15 16:14:12 +0300

    slick slider arrows fixed

--- collsilver/static/js/main.js
+++ collsilver/static/js/main.js
@@ -29,7 +29,8 @@ $(function(){
             slidesToShow: 1,
             slidesToScroll: 1,
             autoplay: true,
-            autoplaySpeed: 6000
+            autoplaySpeed: 6000,
+            arrows: false
         });
     });
 });	
